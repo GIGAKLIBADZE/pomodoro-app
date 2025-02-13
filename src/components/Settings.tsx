76680e6461a310d2608d1fc6ec2df5973cd5ab77
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-13 17:41:59 +0400

    add setting styles

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,7 +5,7 @@ import Down from "/images/icon-arrow-down.svg";
 const Settings: React.FC = () => {
   return (
     <div className="h-full absolute top-0 left-0 for-opacity pt-[4.6rem] px-[2.4rem] font-[KumbhSans]">
-      <div className="w-[32.7rem] h-[54.9rem] rounded-[15px] pt-[2.4rem] bg-white m-auto">
+      <div className="w-[32.7rem] rounded-[15px] pt-[2.4rem] pb-[5.9rem] bg-white m-auto">
         <div className="px-[2.4rem] flex justify-between items-center">
           <h2 className="text-[2rem] font-bold text-[#161932] leading-normal">
             Settings
@@ -17,15 +17,14 @@ const Settings: React.FC = () => {
           />
         </div>
         <div className="line"></div>
-        <h3 className="text-[1.1rem] font-bold tracking-[4.23px] text-center text-[#161932 mt-[2.4rem]">
-          TIME (MINUTES)
-        </h3>
+        <h3 className="sub-title">TIME (MINUTES)</h3>
         <div className="px-[2.4rem] mt-[1.8rem] flex flex-col gap-[0.8rem]">
           <div className="parent-of-input">
             <label htmlFor="pomodoro" className="label-styles">
               pomodoro
             </label>
             <input
+              type="number"
               id="pomodoro"
               name="pomodoro"
               min="1"
@@ -42,6 +41,7 @@ const Settings: React.FC = () => {
               short break
             </label>
             <input
+              type="number"
               id="short"
               name="short"
               min="1"
@@ -58,6 +58,7 @@ const Settings: React.FC = () => {
               long break
             </label>
             <input
+              type="number"
               id="long"
               name="long"
               min="10"
@@ -72,18 +73,29 @@ const Settings: React.FC = () => {
         </div>
         <div className="line"></div>
         <div>
-          <h3>FONT</h3>
-          <div></div>
-          <div></div>
-          <div></div>
+          <h3 className="sub-title">FONT</h3>
+          <div className="fonts-and-colors-container">
+            <div className="font-container font-[KumbhSans] font-normal">
+              Aa
+            </div>
+            <div className="font-container font-[RobotoSlab] font-normal">
+              Aa
+            </div>
+            <div className="font-container font-[SpaceMono] font-bold">Aa</div>
+          </div>
         </div>
+        <div className="line"></div>
         <div>
-          <h3>COLOR</h3>
-          <div></div>
-          <div></div>
-          <div></div>
+          <h3 className="sub-title mt-[1.6rem]">COLOR</h3>
+          <div className="fonts-and-colors-container">
+            <div className="color-container bg-[#f87070]"></div>
+            <div className="color-container bg-[#70f3f8]"></div>
+            <div className="color-container bg-[#d881f8]"></div>
+          </div>
         </div>
-        <button>Apply</button>
+      </div>
+      <div className="flex justify-center relative">
+        <button className="button-styles absolute top-[-2.65rem]">Apply</button>
       </div>
     </div>
   );
