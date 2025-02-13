e883e269333cef44dcbf6f28cba5111c4241678d
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-13 12:03:07 +0400

    configure the routing system in the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import Pomodoro from "./pages/pomodoro";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/pomodoro",
+    element: <Pomodoro />,
+  },
+  {
+    path: "/",
+    element: <Navigate to="/pomodoro" replace />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/pomodoro.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pomodoro: React.FC = () => {
+  return <div>Pomodoro</div>;
+};
+
+export default Pomodoro;
